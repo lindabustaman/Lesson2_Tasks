a6a84c6832b39cb03eef12bc22703a25f0f6ac9a
lindabustaman <lindabustaman@gmail.com>
2024-10-25 00:03:11 -0500

    Initial Commit

---
+++ Simple_Text_Adventure_Game/Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+enum dating_partner
+{
+	alien, 
+	satan, 
+	human, 
+	narrator
+};
+
+enum alien_choices
+{
+	alien_confess,
+	betrayal,
+	return_home
+};
+
+enum satan_choices
+{
+	satan_confess, 
+	sell_soul,
+	fanfiction
+};
+
+enum human_choices
+{
+	human_confess,
+	hard_to_get,
+	reveal_yourself
+};
+
+enum narrator_choices
+{
+	rejection
+};
+
+main()
+{
+	
+
+	
+}
